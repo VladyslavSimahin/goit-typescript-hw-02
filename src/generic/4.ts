b3e48d27fa1496ce082fc444a55ded508aa82455
VladyslavSimahin <vladyslav.simahin@gmail.com>
2024-04-13 20:57:58 -0600

    new

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,15 +1,15 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
-
-class Component {
-  constructor (public props:T) {
-
-  }
+interface IProps {
+  title: string;
+}
+class Component<T> {
+  constructor(public props: T) {}
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<IProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,19 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<TKey, TValue> {
+  key: TKey;
+  value: TValue;
 }
+
+const pair1: KeyValuePair<string, string> = {
+  key: "name",
+  value: " Alice",
+};
+
+const pair2: KeyValuePair<number, boolean> = {
+  key: 777,
+  value: true,
+};
 
 export {};
